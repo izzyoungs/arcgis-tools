2f5c7eedfafac4d81d57275b43e6478beb9a47d5
Izzy Youngs <60447554+izzyoungs@users.noreply.github.com>
2024-08-28 14:51:23 -0700

    Add tool testing aprx

--- Scripts/GuessCRS.R
+++ Scripts/GuessCRS.R
@@ -1,9 +1,12 @@
 tool_exec<- function(in_params, out_params){
 
-  require(sf)
-  require(crsuggest)
+  suppressMessages(require(sf))
+  suppressMessages(require(crsuggest))
   
   input_xy <- in_params[[1]]
+  crs_string <- in_params[[2]]
+  crs_object <- st_crs(crs_string)
+  wkt <- arc.fromWktToP4(crs_object$wkt)
 
   arc.progress_label("Evaluating CRS...")
   arc.progress_pos(40)
@@ -13,9 +16,8 @@ tool_exec<- function(in_params, out_params){
   bbox_matrix <- matrix(c(coords[1], coords[2], coords[3], coords[4]), nrow = 2, byrow = TRUE)
   centroid_x <- mean(bbox_matrix[, 1])
   centroid_y <- mean(bbox_matrix[, 2])
-  centroid_point <- st_sfc(st_point(c(centroid_x, centroid_y)), crs = 3857)
-  centroid_wgs84 <- st_transform(centroid_point, crs = 4326)
-  crs_suggestions <- suggest_crs(centroid_wgs84)
+  centroid_point <- st_sfc(st_point(c(centroid_x, centroid_y)), crs = wkt)
+  crs_suggestions <- suggest_crs(centroid_point)
   
   print(crs_suggestions)
 }
