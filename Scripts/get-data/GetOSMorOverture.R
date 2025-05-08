611e3716b618580313d7b80ade123972dd5d3507
Izzy Youngs <60447554+izzyoungs@users.noreply.github.com>
2025-05-08 16:01:49 -0700

    Update GetOSMorOverture.R
    Update for Overture API

--- Scripts/get-data/GetOSMorOverture.R
+++ Scripts/get-data/GetOSMorOverture.R
@@ -21,11 +21,8 @@ tool_exec <- function(in_params, out_params) {
   study_area <- arc.open(in_params[[1]]) |> arc.select()
   data_source <- in_params[[2]]  # "OSM", "Overture", or "Both"
   feature_types <- in_params[[3]]  # "Amenities", "Streets", or "Buildings"
-  output_gdb <- in_params[[4]]
   
   output <- out_params[[1]]
-  
-  print(output_gdb)
   
   # unlist the feature types from boolean to "Amenities", "Streets", or "Buildings"
   all_options <- c("Amenities", "Streets", "Buildings")
@@ -37,17 +34,25 @@ tool_exec <- function(in_params, out_params) {
   print("Extracting bounding box from input study area...")
   
   # Convert study area to sf and get bounding box
-  study_area_sf <- arc.data2sf(study_area)
+  study_area_sf <- arc.data2sf(study_area) |>
+    st_transform(4326) |> 
+    st_make_valid()
   bbox <- st_bbox(study_area_sf)
   
   arc.progress_pos(30)
+  
+
+# OSM ---------------------------------------------------------------------
   
   if (any(data_source %in% c("OSM", "Both"))) {
     
     osm_query <- opq(bbox = bbox)
     
+
+## OSM Amenities -----------------------------------------------------------
+    
     # Process different geometry types
-    if (selected_type %in% "Amenities") {
+    if ("Amenities" %in% selected_type) {
       
       print("Fetching OSM amenities...")
       
@@ -60,10 +65,13 @@ tool_exec <- function(in_params, out_params) {
         st_make_valid()
       
       print("Writing OSM amenities to output geodatabase...")
-      arc.write(file.path(output_gdb, "OSM_Amenities"), osm_amentities_sf, overwrite = TRUE)
+      arc.write(paste0(output, "_OSM_Amenities"), osm_amentities_sf, overwrite = TRUE)
       
     }
-    if (selected_type %in% "Streets") {
+
+# OSM Streets -------------------------------------------------------------    
+
+    if ("Streets" %in% selected_type) {
       
       print("Fetching OSM streets...")
       
@@ -76,10 +84,13 @@ tool_exec <- function(in_params, out_params) {
         st_make_valid()
       
       print("Writing OSM streets to output geodatabase...")
-      arc.write(file.path(output_gdb, "OSM_Streets"), osm_streets_sf, overwrite = TRUE)
+      arc.write(paste0(output, "_OSM_Streets"), osm_streets_sf, overwrite = TRUE)
       
     }
-    if (selected_type %in% "Buildings") {
+    
+# OSM Buildings -----------------------------------------------------------
+
+    if ("Buildings" %in% selected_type) {
       
       print("Fetching OSM buildings...")
       
@@ -92,76 +103,97 @@ tool_exec <- function(in_params, out_params) {
         st_make_valid()
       
       print("Writing OSM buildings to output geodatabase...")
-      arc.write(file.path(output_gdb, "/OSM_Buildings"), osm_buildings_sf, overwrite = TRUE)
+      arc.write(paste0(output, "_OSM_Buildings"), osm_buildings_sf, overwrite = TRUE)
     }
   }
+  
+# Overture ---------------------------------------------------------------
   
   if (any(data_source %in% c("Overture", "Both"))) {
     
     bbox_overture <- bbox |>
       as.vector()
     
-    if (selected_type %in% "Amenities") {
+
+## Overture Amenities ------------------------------------------------------
+    
+    if ("Amenities" %in% selected_type) {
       
       print("Fetching Overture amenities...")
       
       suppressWarnings({
-        overture_amentiy <- record_batch_reader(overture_type = 'place', bbox = bbox_overture)
-        overture_amentiy_sf <- as.data.frame(overture_amentiy)
-        overture_amentiy_sf$name <- overture_amentiy_sf$names[[1]]
-        overture_amentiy_sf$category <- overture_amentiy_sf$categories[[1]]
+        overture_amentiy <- record_batch_reader(schema_type = 'place', bbox = bbox_overture) |>
+          suppressMessages()
         
-        overture_amentiy_sf_cleaned <- overture_amentiy_sf |> 
-          select(c('id', 'geometry', 'name', 'category', 'confidence'))
+        overture_amentiy_sf <- overture_amentiy |>
+          mutate(geometry = st_as_sfc(geometry, EWKB = TRUE),
+                 name = names[[1]],
+                 primary_category = categories$primary, 
+                 secondary_categories = purrr::map_chr(categories$alternate, ~ paste(.x, collapse = ", ")),
+                 confidence = confidence[[1]]) |>
+          select(id, name, primary_category, secondary_categories, confidence, geometry) |>
+          st_as_sf(crs = 4326)
+
       })
-      
-      print("Writing Overture amenities to output geodatabase...")
-      arc.write(file.path(output_gdb, "/Overture_Amenities"), overture_amentiy_sf_cleaned, overwrite = TRUE)
+
+      if (nrow(overture_amentiy_sf) == 0) {
+        print("No amenities found in the specified bounding box in Overture.")
+      } else {
+        print("Writing Overture amenities to output geodatabase...")
+        arc.write(paste0(output, "_Overture_Amenities"), overture_amentiy_sf, overwrite = TRUE)
+      }
     
     }
-    if (selected_type %in% "Streets") {
+    
+# Overture Streets -------------------------------------------------------
+
+    if ("Streets" %in% selected_type) {
       
       print("Fetching Overture streets...")
       
       suppressWarnings({
-        overture_streets <- record_batch_reader(overture_type = 'segments', bbox = bbox_overture)
-        overture_streets_sf <- as.data.frame(overture_streets)
+        overture_streets <- record_batch_reader(schema_type = 'segment', bbox = bbox_overture) |>
+          suppressMessages()
         
-        overture_streets_sf$name <- overture_streets_sf$name[[1]]
-        overture_streets_sf$class <- overture_streets_sf$class[[1]]
+        overture_streets_sf <- overture_streets |>
+          mutate(name = names[[1]],
+                 class = class[[1]]) |>
+          select(id, name, class, geometry) |>
+          st_as_sf(crs = 4326)
         
-        overture_streets_cleaned <- overture_streets_sf |> 
-          select(c('id', 'geometry', 'name', 'class', 'confidence'))
       })
       
-      print("Writing Overture streets to output geodatabase...")
-      arc.write(file.path(output_gdb, "/Overture_Streets"), overture_streets_cleaned, overwrite = TRUE)
+      if (nrow(overture_streets_sf) == 0) {
+        print("No streets found in the specified bounding box in Overture.")
+      } else {
+        print("Writing Overture streets to output geodatabase...")
+        arc.write(paste0(output, "_Overture_Streets"), overture_streets_sf, overwrite = TRUE)
+      }
     }
     
-    if (selected_type %in% "Buildings") {
+# Overture Buildings -----------------------------------------------------
+    
+    if ("Buildings" %in% selected_type) {
       
       print("Fetching Overture buildings...")
       
       suppressWarnings({
-        overture_buildings <- record_batch_reader(overture_type = 'building', bbox = bbox_overture)
-        overture_buildings_sf <- as.data.frame(overture_buildings)
+        overture_buildings <- record_batch_reader(schema_type = 'building', bbox = bbox_overture)
         
-        overture_buildings_sf$name <- overture_buildings_sf$name[[1]]
-        overture_buildings_sf$subtype <- overture_buildings_sf$subtype[[1]]
-        overture_buildings_sf$height <- overture_buildings_sf$height[[1]]
-        overture_buildings_sf$source <- overture_buildings_sf$source[[1]]
-        
-        overture_buildings_cleaned <- overture_buildings_sf |> 
-          select(c('id', 'geometry', 'name', 'subtype', 'height', 'source', 'confidence'))
+        overture_buildings_sf <- overture_buildings |>
+          mutate(name = names[[1]],
+                 subtype = subtype[[1]], 
+                 height = height[[1]],
+                 source = sources[[1]]$dataset[[1]],
+                 confidence = sources[[1]]$confidence[[1]]) |>
+          select(id, name, subtype, height, source, confidence, geometry) |>
+          st_as_sf(crs = 4326)
       })
       
       print("Writing Overture buildings to output geodatabase...")
-      arc.write(file.path(output_gdb, "/Overture_Buildings"), overture_buildings_cleaned, overwrite = TRUE)
+      arc.write(paste0(output, "_Overture_Buildings"), overture_buildings_sf, overwrite = TRUE)
     }
   }
-
-
-  # out_params[[1]] <- output_fcs
   
   arc.progress_pos(100)
   print("Process complete.")
